3e6f4fbb08f2639d28465dbf6823648104e743dd
Alvaro Capaceta <ingcapadev@gmail.com>
2023-05-24 11:42:52 -0700

    Getters and Setters Calculator

--- app/src/main/java/com/example/javacalculadora/Calculator.java
+++ app/src/main/java/com/example/javacalculadora/Calculator.java
@@ -1,8 +1,25 @@
 package com.example.javacalculadora;
 
 public class Calculator {
-    private final int num1;
-    private final int num2;
+    private int num1;
+    private int num2;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum1(int num1){
+        this.num1 = num1;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
 
     public Calculator(int num1, int num2){
         this.num1 = num1;
